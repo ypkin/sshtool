eabd894f2c12a511f0a9fe8d87095747a861ea52
ypkin <59020573+ypkin@users.noreply.github.com>
2024-10-25 14:20:07 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -29,86 +29,82 @@ function show_warp_menu {
     echo -e "${PINK}1.${RESET} ${GREEN}WARP安装${RESET}\n"
     echo -e "${PINK}2.${RESET} ${GREEN}WARP Go安装${RESET}\n"
     echo -e "${PINK}3.${RESET} ${GREEN}YGKKK WARP脚本${RESET}\n"
-    echo -e "${PINK}4.${RESET} ${GREEN}返回主菜单${RESET}\n"
 }
 
-# 主菜单循环
-while true; do
-    show_main_menu
-    echo -e "请输入你想执行的命令序号（1-15）："
-    read choice
+# 主菜单
+show_main_menu
+echo -e "请输入你想执行的命令序号（1-15）："
+read choice
 
-    # 执行相应的命令
-    case $choice in
-        1)
-            apt update -y && apt install -y curl && apt install -y socat && apt install -y wget -y
-            ;;
-        2)
-            curl -fsSL https://raw.githubusercontent.com/eooce/ssh_tool/main/ssh_tool.sh -o ssh_tool.sh && chmod +x ssh_tool.sh && ./ssh_tool.sh
-            ;;
-        3)
-            curl -sS -O https://kejilion.pro/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
-            ;;
-        4)
-            curl -O https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh
-            ;;
-        5)
-            bash <(curl https://raw.githubusercontent.com/xykt/Utilities/main/nezha/ipv6flag.sh)
-            ;;
-        6)
-            wget -O hy2.sh https://raw.githubusercontent.com/zrlhk/alpine-hysteria2/main/hy2.sh && sh hy2.sh
-            ;;
-        7)
-            wget -O hy2.sh https://raw.githubusercontent.com/ypkin/hysteria-setup/refs/heads/main/hy2.sh && chmod +x hy2.sh && ./hy2.sh
-            ;;
-        8)
-            bash <(curl -L -s media.ispvps.com)
-            ;;
-        9)
-            bash <(curl -Ls https://gitlab.com/rwkgyg/sing-box-yg/raw/main/sb.sh)
-            ;;
-        10)
-            bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
-            ;;
-        11)
-            apk add curl && apk add bash && bash <(curl -Ls https://raw.githubusercontent.com/Lynn-Becky/Alpine-x-ui/main/alpine-xui.sh)
-            ;;
-        12)
-            curl -L https://gitlab.com/spiritysdx/Oracle-server-keep-alive-script/-/raw/main/oalive.sh -o oalive.sh && chmod +x oalive.sh && bash oalive.sh
-            ;;
-        13)  # WARP 二级菜单
-            while true; do
-                show_warp_menu
-                echo -e "请输入你想执行的WARP命令序号（1-4）："
-                read warp_choice
-                case $warp_choice in
-                    1)
-                        wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh 4
-                        ;;
-                    2)
-                        wget -N https://gitlab.com/fscarmen/warp/-/raw/main/warp-go.sh && bash warp-go.sh
-                        ;;
-                    3)
-                        bash <(wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh 2> /dev/null)
-                        ;;
-                    4)
-                        break  # 返回主菜单
-                        ;;
-                    *)
-                        echo "无效的序号，请输入 1 到 4 之间的数字。"
-                        ;;
-                esac
-            done
-            ;;
-        14)
-            wget -N --no-check-certificate https://raw.githubusercontent.com/Chennhaoo/Shell_Bash/master/AutoTrace.sh && chmod +x AutoTrace.sh && bash AutoTrace.sh
-            ;;
-        15)
-            echo "退出脚本。"
-            exit 0
-            ;;
-        *)
-            echo "无效的序号，请输入 1 到 15 之间的数字。"
-            ;;
-    esac
-done
+# 执行相应的命令
+case $choice in
+    1)
+        apt update -y && apt install -y curl && apt install -y socat && apt install -y wget -y
+        ;;
+    2)
+        curl -fsSL https://raw.githubusercontent.com/eooce/ssh_tool/main/ssh_tool.sh -o ssh_tool.sh && chmod +x ssh_tool.sh && ./ssh_tool.sh
+        ;;
+    3)
+        curl -sS -O https://kejilion.pro/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
+        ;;
+    4)
+        curl -O https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh
+        ;;
+    5)
+        bash <(curl https://raw.githubusercontent.com/xykt/Utilities/main/nezha/ipv6flag.sh)
+        ;;
+    6)
+        wget -O hy2.sh https://raw.githubusercontent.com/zrlhk/alpine-hysteria2/main/hy2.sh && sh hy2.sh
+        ;;
+    7)
+        wget -O hy2.sh https://raw.githubusercontent.com/ypkin/hysteria-setup/refs/heads/main/hy2.sh && chmod +x hy2.sh && ./hy2.sh
+        ;;
+    8)
+        bash <(curl -L -s media.ispvps.com)
+        ;;
+    9)
+        bash <(curl -Ls https://gitlab.com/rwkgyg/sing-box-yg/raw/main/sb.sh)
+        ;;
+    10)
+        bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+        ;;
+    11)
+        apk add curl && apk add bash && bash <(curl -Ls https://raw.githubusercontent.com/Lynn-Becky/Alpine-x-ui/main/alpine-xui.sh)
+        ;;
+    12)
+        curl -L https://gitlab.com/spiritysdx/Oracle-server-keep-alive-script/-/raw/main/oalive.sh -o oalive.sh && chmod +x oalive.sh && bash oalive.sh
+        ;;
+    13)
+        # WARP 二级菜单
+        show_warp_menu
+        echo -e "请输入你想执行的WARP命令序号（1-3）："
+        read warp_choice
+        case $warp_choice in
+            1)
+                wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh 4
+                ;;
+            2)
+                wget -N https://gitlab.com/fscarmen/warp/-/raw/main/warp-go.sh && bash warp-go.sh
+                ;;
+            3)
+                bash <(wget -qO- https://gitlab.com/rwkgyg/CFwarp/raw/main/CFwarp.sh 2> /dev/null)
+                ;;
+            *)
+                echo "无效的WARP选项，退出脚本。"
+                ;;
+        esac
+        ;;
+    14)
+        wget -N --no-check-certificate https://raw.githubusercontent.com/Chennhaoo/Shell_Bash/master/AutoTrace.sh && chmod +x AutoTrace.sh && bash AutoTrace.sh
+        ;;
+    15)
+        echo "退出脚本。"
+        exit 0
+        ;;
+    *)
+        echo "无效的序号，请输入 1 到 15 之间的数字。"
+        ;;
+esac
+
+# 命令执行完后退出脚本
+exit 0
